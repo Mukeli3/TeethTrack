74770b9a5aa39b5e123ab34fff2b0c76948e326d
Mukeli3 <mukelicynthia20@gmail.com>
2024-10-13 02:02:54 +0000

    control imports

---
+++ back_end/app/models/__init__.py
@@ -0,0 +1,6 @@
+from .user_model import User
+from .appointment_model import appointment
+from .patient_model import patient_records
+from .billing_model import billing_table
+
+__all__ = ['User', 'appointment', 'patient_records', 'billing_table']
