bbe96f24e1820ecea0b58bc9a806690cdbdebde9
Mukeli3 <mukelicynthia20@gmail.com>
2024-10-14 16:22:04 +0000

    refactored to acccount for search functionality

--- back_end/app/models/appointment_model.py
+++ back_end/app/models/appointment_model.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -8,3 +9,11 @@ class appointment(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
     appointment = db.Column(db.DateTime, nullable=False)
     status = db.Column(db.String(20), nullable=False)
+
+    @classmethod
+    def search_slots(cls, start_time: datetime, end_time: datetime):
+        return cls.query.filter(
+                cls.appointment >= start_time,
+                cls.appointment <= end_time,
+                cls.status == 'available'
+                ).all()
