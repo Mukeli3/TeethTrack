d3f1cc243b5c8441496037f49f06b3c96c75799e
Mukeli3 <mukelicynthia20@gmail.com>
2024-10-14 16:24:09 +0000

    included search functionality blueprint

--- back_end/app/routes/__init__.py
+++ back_end/app/routes/__init__.py
@@ -1,6 +1,7 @@
-from .auth import auth_bp
+from .auth import auth
 from .patient_route import patient_bp
 from .dentist_route import dentist_bp
 from .billing_route import billing_bp
+from .search_bp import appointments
 
-__all__ = ['auth_bp', 'patient_bp', 'dentist_bp', 'billing_bp']
+__all__ = ['auth', 'patient_bp', 'dentist_bp', 'billing_bp', 'appointments']
