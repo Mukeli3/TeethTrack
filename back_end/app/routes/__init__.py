e674be79c4c026a0025fe26c8524afbc313fd64b
Mukeli3 <mukelicynthia20@gmail.com>
2024-10-15 16:10:23 +0000

    added dashboard blueprint

--- back_end/app/routes/__init__.py
+++ back_end/app/routes/__init__.py
@@ -2,6 +2,7 @@ from .auth import auth
 from .patient_route import patient_bp
 from .dentist_route import dentist_bp
 from .billing_route import billing_bp
-from .search_bp import appointments
+from .search_route import search_bp
+from .dashboard_route import dashboard_bp
 
-__all__ = ['auth', 'patient_bp', 'dentist_bp', 'billing_bp', 'appointments']
+__all__ = ['auth', 'patient_bp', 'dentist_bp', 'billing_bp', 'search_bp', 'dashboard_bp']
