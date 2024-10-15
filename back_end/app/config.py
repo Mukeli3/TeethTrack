8512a55e86e2d025f5c2e9ed8506c2e78db758d2
Mukeli3 <mukelicynthia20@gmail.com>
2024-10-15 18:24:27 +0000

    configuration file

---
+++ back_end/app/config.py
@@ -0,0 +1,46 @@
+import os
+
+class Config:
+    """
+    base configuration class containing default settings
+    """
+    # session and JWT secret key
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    # db configuration
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    
+    # significant overhead silence warning for tracking modifications
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # JWT Configuration
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+    JWT_ACCESS_TOKEN_EXPIRES = 3600
+
+    DEBUG = False
+    TESTING = False
+    ENV = 'production'
+
+
+class DevelopmentConfig(Config):
+    """
+    development environment configuration
+    """
+    DEBUG = True
+    ENV = 'development'
+
+
+class TestingConfig(Config):
+    """
+    testing environment configuration
+    """
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # Use in-memory database for testing
+
+
+class ProductionConfig(Config):
+    """
+    production environment config
+    """
+    DEBUG = False
+    ENV = 'production'
