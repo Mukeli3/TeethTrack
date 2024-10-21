f2a09439e210ae3a0119c737b546d9fe44eb587f
Mukeli3 <mukelicynthia20@gmail.com>
2024-10-21 19:36:38 +0000

    commented dashboard route out

--- back_end/app/__init__.py
+++ back_end/app/__init__.py
@@ -1,11 +1,12 @@
+#!/usr/bin/python3
 import os
+from flask_cors import CORS
 from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate # automate db schema management
 from flask_jwt_extended import JWTManager
-from app.models.user_model import User
 
 
 load_dotenv()
@@ -25,9 +26,11 @@ def create_app():
          the configured flask app instancei
     """
     app = Flask(__name__, static_folder='static')
+    #CORS(app)
 
     app.config.from_object('app.config.Config')
     db.init_app(app) # initialize SQLAlchemy with the app
+    jwt.init_app(app)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
@@ -46,16 +49,17 @@ def create_app():
     from app.routes.dentist_route import dentist_bp
     from app.routes.billing_route import billing_bp
     from app.routes.search_route import search_bp
-    from app.routes.dashboard_route import dashboard_bp
+    # from app.routes.dashboard_route import dashboard_bp
 
     app.register_blueprint(auth)
     app.register_blueprint(patient_bp)
     app.register_blueprint(dentist_bp)
     app.register_blueprint(billing_bp)
     app.register_blueprint(search_bp)
-    app.register_blueprint(dashboard_bp)
+    # app.register_blueprint(dashboard_bp)
 
     with app.app_context():
+        from app.models import User, Appointment, PatientRecords, Billing
         db.create_all() # create db tables
 
-    return app 
+    return app
