9c125cdaf0123dcf18160cb7cc3041bd2de09789
Mukeli3 <mukelicynthia20@gmail.com>
2024-10-21 19:35:17 +0000

    removed dashboard route

--- back_end/app/routes/__init__.py
+++ back_end/app/routes/__init__.py
@@ -3,6 +3,6 @@ from .patient_route import patient_bp
 from .dentist_route import dentist_bp
 from .billing_route import billing_bp
 from .search_route import search_bp
-from .dashboard_route import dashboard_bp
+# from .dashboard_route import dashboard_bp
 
-__all__ = ['auth', 'patient_bp', 'dentist_bp', 'billing_bp', 'search_bp', 'dashboard_bp']
+__all__ = ['auth', 'patient_bp', 'dentist_bp', 'billing_bp', 'search_bp']
